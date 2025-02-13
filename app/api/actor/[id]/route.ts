53c7ad4394c41bbd5f8f9237de6d85e9c69acc5a
thathsaraDinu <wthathsaradinuwan@gmail.com>
2025-02-13 17:24:05 +0530

    refactor: remove commented-out code and improve formatting in actor and movies routes

--- app/api/actor/[id]/route.ts
+++ app/api/actor/[id]/route.ts
@@ -7,7 +7,7 @@ export async function GET(
   req: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
-    const { id } = await params;
+  const { id } = await params;
 
   if (!id) {
     return NextResponse.json(
