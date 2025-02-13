146283b6fcb7f9897e3d2a055f1dff5dee22c31c
thathsaraDinu <wthathsaradinuwan@gmail.com>
2025-02-13 17:15:05 +0530

    refactor: update parameter type to promise for actor route GET method

--- app/api/actor/[id]/route.ts
+++ app/api/actor/[id]/route.ts
@@ -5,7 +5,7 @@ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
     const { id } = await params;
 
